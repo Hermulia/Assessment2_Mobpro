b5337dc92b4f6ab6f9a96cbb66f2efc7e1f6deb3
Anjelita <hermuliaanjelita@gmail.com>
2025-05-11 01:36:59 +0700

    mainviewmodel

---
+++ app/src/main/java/com/anjelitahp0044/expensestracker_assessment2/screen/MainViewModel.kt
@@ -0,0 +1,36 @@
+package com.anjelitahp0044.expensestracker_assessment2.screen
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.anjelitahp0044.expensestracker_assessment2.database.PengeluaranDao
+import com.anjelitahp0044.expensestracker_assessment2.model.Pengeluaran
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
+
+class MainViewModel(dao: PengeluaranDao) : ViewModel() {
+
+    val data: StateFlow<List<Pengeluaran>> = dao.getPengeluaran().stateIn(
+        scope = viewModelScope,
+        started = SharingStarted.WhileSubscribed(9000L),
+        initialValue = emptyList()
+
+    )
+    fun restore(id: Long) {
+        viewModelScope.launch {
+            dao.restoreById(id)
+        }
+    }
+
+    fun delete(id: Long) {
+        viewModelScope.launch {
+            val timestamp = SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault()).format(Date())
+            dao.softDeleteById(id, timestamp)
+        }
+    }
+}
+
