a03d7cd0bfd11ca7c10992d68b889d42bfb1047c
Anjelita <hermuliaanjelita@gmail.com>
2025-05-11 01:37:49 +0700

    pengeluarandb

--- app/src/main/java/com/anjelitahp0044/expensestracker_assessment2/database/PengeluaranDb.kt
+++ app/src/main/java/com/anjelitahp0044/expensestracker_assessment2/database/PengeluaranDb.kt
@@ -4,6 +4,8 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.room.migration.Migration
+import androidx.sqlite.db.SupportSQLiteDatabase
 import com.anjelitahp0044.expensestracker_assessment2.model.Pengeluaran
 
 @Database(entities = [Pengeluaran::class], version = 1, exportSchema = false)
@@ -17,19 +19,24 @@ abstract class PengeluaranDb : RoomDatabase() {
         private var INSTANCE: PengeluaranDb? = null
 
         fun getInstance(context: Context): PengeluaranDb {
-            synchronized(this) {
+            return INSTANCE ?: synchronized(this) {
                 var instance = INSTANCE
-
-                if (instance == null) {
-                    instance = Room.databaseBuilder(
-                        context.applicationContext,
-                        PengeluaranDb::class.java,
-                        "pengeluaran.db"
-                    ).build()
-                    INSTANCE = instance
-                }
-                return instance
+                instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    PengeluaranDb::class.java,
+                    "pengeluaran.db"
+                )
+                    .addMigrations(MIGRATION_DB)
+                    .build()
+                INSTANCE = instance
+                instance
+            }
+        }
+               private val MIGRATION_DB = object : Migration(1, 2) {
+                   override fun migrate(db: SupportSQLiteDatabase) {
+//                       database.execSQL("ALTER TABLE pengeluaran ADD COLUMN isDeleted INTEGER NOT NULL DEFAULT 0")
+//                       database.execSQL("ALTER TABLE pengeluaran ADD COLUMN deletedAt TEXT")
+                   }
             }
         }
     }
-}
