546f838688de5aab4cdd74fa45edde48d60b1b5d
Anjelita <hermuliaanjelita@gmail.com>
2025-05-12 12:27:35 +0700

    6

--- app/src/main/java/com/anjelitahp0044/expensestracker_assessment2/navigation/NavGraph.kt
+++ app/src/main/java/com/anjelitahp0044/expensestracker_assessment2/navigation/NavGraph.kt
@@ -2,31 +2,34 @@ package com.anjelitahp0044.expensestracker_assessment2.navigation
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
-import androidx.compose.ui.graphics.BlendMode.Companion.Screen
 import androidx.navigation.NavHostController
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
 import com.anjelitahp0044.expensestracker_assessment2.screen.DetailScreen
 import com.anjelitahp0044.expensestracker_assessment2.screen.KEY_ID_PENGELUARAN
 import com.anjelitahp0044.expensestracker_assessment2.screen.MainScreen
+import com.anjelitahp0044.expensestracker_assessment2.screen.RecycleBinScreen
+
 
 @Composable
-fun SetupNavGraph(navController: NavHostController = rememberNavController(), isDarkTheme: MutableState<Boolean>) {
+fun SetupNavGraph(navController: NavHostController = rememberNavController(), isDarkTheme: MutableState<Boolean>){
     NavHost(
-        navController = navController,
+        navController=navController,
         startDestination = Screen.Home.route
-    ) {
-        composable(route = Screen.Home.route) {
+    ){
+        composable(route = Screen.Home.route){
             MainScreen(navController = navController, isDarkTheme = isDarkTheme)
         }
-        composable(route = Screen.FormBaru.route ) {
+        composable(route = Screen.FormBaru.route) {
             DetailScreen(navController = navController, isDarkTheme = isDarkTheme)
         }
         composable(route = Screen.RecycleBin.route) {
             RecycleBinScreen(navController)
         }
+
         composable(
             route = Screen.FormUbah.route,
             arguments = listOf(
@@ -35,7 +38,8 @@ fun SetupNavGraph(navController: NavHostController = rememberNavController(), is
         ) { navBackStackEntry ->
             val id = navBackStackEntry.arguments?.getLong(KEY_ID_PENGELUARAN)
             DetailScreen(navController = navController, id = id, isDarkTheme = isDarkTheme)
-
         }
+
+
     }
 }
