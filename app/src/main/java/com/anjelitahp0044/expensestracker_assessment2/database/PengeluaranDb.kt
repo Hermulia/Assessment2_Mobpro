1519353968f02a6106a013d5c0c528c25efa7c0b
Anjelita <hermuliaanjelita@gmail.com>
2025-05-12 12:27:47 +0700

    8

--- app/src/main/java/com/anjelitahp0044/expensestracker_assessment2/database/PengeluaranDb.kt
+++ app/src/main/java/com/anjelitahp0044/expensestracker_assessment2/database/PengeluaranDb.kt
@@ -1,42 +1,38 @@
 package com.anjelitahp0044.expensestracker_assessment2.database
 
 import android.content.Context
-import androidx.room.Database
-import androidx.room.Room
-import androidx.room.RoomDatabase
+import androidx.room.*
 import androidx.room.migration.Migration
 import androidx.sqlite.db.SupportSQLiteDatabase
 import com.anjelitahp0044.expensestracker_assessment2.model.Pengeluaran
 
-@Database(entities = [Pengeluaran::class], version = 1, exportSchema = false)
+@Database(entities = [Pengeluaran::class], version = 2)
 abstract class PengeluaranDb : RoomDatabase() {
-
     abstract val dao: PengeluaranDao
 
     companion object {
-
         @Volatile
         private var INSTANCE: PengeluaranDb? = null
 
         fun getInstance(context: Context): PengeluaranDb {
             return INSTANCE ?: synchronized(this) {
-                var instance = INSTANCE
-                instance = Room.databaseBuilder(
+                val instance = Room.databaseBuilder(
                     context.applicationContext,
                     PengeluaranDb::class.java,
                     "pengeluaran.db"
                 )
-                    .addMigrations(MIGRATION_DB)
+                    .addMigrations(MIGRATION_1_2)
                     .build()
                 INSTANCE = instance
                 instance
             }
         }
-               private val MIGRATION_DB = object : Migration(1, 2) {
-                   override fun migrate(db: SupportSQLiteDatabase) {
-//                       database.execSQL("ALTER TABLE pengeluaran ADD COLUMN isDeleted INTEGER NOT NULL DEFAULT 0")
-//                       database.execSQL("ALTER TABLE pengeluaran ADD COLUMN deletedAt TEXT")
-                   }
+
+        private val MIGRATION_1_2 = object : Migration(1, 2) {
+            override fun migrate(database: SupportSQLiteDatabase) {
+                database.execSQL("ALTER TABLE pengeluaran ADD COLUMN isDeleted INTEGER NOT NULL DEFAULT 0")
+                database.execSQL("ALTER TABLE pengeluaran ADD COLUMN deletedAt TEXT")
             }
         }
     }
+}
