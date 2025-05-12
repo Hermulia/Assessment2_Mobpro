fef83b3231d641ee52b2d10a1d87ddf81a87fafa
Anjelita <hermuliaanjelita@gmail.com>
2025-05-12 16:16:56 +0700

    themes

--- app/src/main/java/com/anjelitahp0044/expensestracker_assessment2/ui/theme/Color.kt
+++ app/src/main/java/com/anjelitahp0044/expensestracker_assessment2/ui/theme/Color.kt
@@ -1,11 +1,21 @@
-package com.anjelitahp0044.expensestracker_assessment2.ui.theme
-
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+// Light Theme
+val SoftIndigo = Color(0xFF7768AE)
+val LightLavender = Color(0xFFA98CC8)
+val DustyPink = Color(0xFFE3A8C4)
+val PeachCream = Color(0xFFFFE6E0)
+val DeepPlumGray = Color(0xFF52415A)
+val DustyRose = Color(0xFFCDAAA9)
+val MauveBrown = Color(0xFF9B7C7C)
+val OrangeSnackbar = Color(0xFFF57C00)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+// Dark Theme
+val SoftIndigoDark = Color(0xFF5F5391)
+val LavenderDark = Color(0xFF9273B6)
+val DustyPinkDark = Color(0xFFC087A4)
+val PeachCreamDark = Color(0xFFEBC6A7)
+val DeepPlumGrayDark = Color(0xFF3B2E3F)
+val DustyRoseDark = Color(0xFFA17D7B)
+val MauveBrownDark = Color(0xFF7C6362)
+val OrangeDarkSnackbar = Color(0xFFFF9800)
--- app/src/main/java/com/anjelitahp0044/expensestracker_assessment2/ui/theme/Theme.kt
+++ app/src/main/java/com/anjelitahp0044/expensestracker_assessment2/ui/theme/Theme.kt
@@ -1,58 +1,64 @@
 package com.anjelitahp0044.expensestracker_assessment2.ui.theme
 
-import android.app.Activity
-import android.os.Build
+import DeepPlumGray
+import DeepPlumGrayDark
+import DustyPink
+import DustyPinkDark
+import DustyRose
+import DustyRoseDark
+import LavenderDark
+import LightLavender
+import MauveBrown
+import MauveBrownDark
+import OrangeDarkSnackbar
+import OrangeSnackbar
+import PeachCream
+import PeachCreamDark
+import SoftIndigo
+import SoftIndigoDark
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.platform.LocalContext
 
-private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+private val LightColors = lightColorScheme(
+    primary = SoftIndigo,
+    onPrimary = DeepPlumGray,
+    secondary = LightLavender,
+    onSecondary = DeepPlumGray,
+    background = PeachCream,
+    onBackground = DeepPlumGray,
+    surface = DustyRose,
+    onSurface = DeepPlumGray,
+    tertiary = DustyPink,
+    error = OrangeSnackbar,
+    outline = MauveBrown
 )
 
-private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+private val DarkColors = darkColorScheme(
+    primary = SoftIndigoDark,
+    onPrimary = DeepPlumGrayDark,
+    secondary = LavenderDark,
+    onSecondary = DeepPlumGrayDark,
+    background = PeachCreamDark,
+    onBackground = DeepPlumGrayDark,
+    surface = DustyRoseDark,
+    onSurface = DeepPlumGrayDark,
+    tertiary = DustyPinkDark,
+    error = OrangeDarkSnackbar,
+    outline = MauveBrownDark
 )
 
 @Composable
 fun ExpensesTracker_Assessment2Theme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
-    val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
-
-        darkTheme -> DarkColorScheme
-        else -> LightColorScheme
-    }
-
     MaterialTheme(
-        colorScheme = colorScheme,
+        colorScheme = if (darkTheme) DarkColors else LightColors,
         typography = Typography,
+        shapes = Shapes,
         content = content
     )
 }
