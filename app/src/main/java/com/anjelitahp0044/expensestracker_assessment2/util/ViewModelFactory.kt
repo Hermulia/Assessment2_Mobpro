0a24486b12e65ef91ccb60ed0a120ed2f071ce1f
Anjelita <hermuliaanjelita@gmail.com>
2025-05-12 12:27:53 +0700

    9

--- app/src/main/java/com/anjelitahp0044/expensestracker_assessment2/util/ViewModelFactory.kt
+++ app/src/main/java/com/anjelitahp0044/expensestracker_assessment2/util/ViewModelFactory.kt
@@ -3,19 +3,16 @@ package com.anjelitahp0044.expensestracker_assessment2.util
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.anjelitahp0044.expensestracker_assessment2.database.PengeluaranDao
-import com.anjelitahp0044.expensestracker_assessment2.database.PengeluaranDb
 import com.anjelitahp0044.expensestracker_assessment2.screen.DetailViewModel
 import com.anjelitahp0044.expensestracker_assessment2.screen.MainViewModel
 
-class ViewModelFactory(
-    private val context: PengeluaranDao
-) : ViewModelProvider.Factory {
-    @Suppress("uncheck_cast")
-    override fun <T : ViewModel> create(modelClass: Class<T>): T {
-        val dao = PengeluaranDb.getInstance(context).dao
-        if (modelClass.isAssignableFrom(MainViewModel::class.java)) {
+class ViewModelFactory (
+    private val dao :PengeluaranDao): ViewModelProvider.Factory{
+    @Suppress("unchecked_cast")
+    override fun <T: ViewModel>create(modelClass: Class<T>):T{
+        if (modelClass.isAssignableFrom(MainViewModel::class.java)){
             return MainViewModel(dao) as T
-        } else if (modelClass.isAssignableFrom(DetailViewModel::class.java)) {
+        }else if (modelClass.isAssignableFrom(DetailViewModel::class.java)){
             return DetailViewModel(dao) as T
         }
         throw IllegalArgumentException("Unknown ViewModel class")
